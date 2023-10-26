e2e04e15f1d671f6fe8344483c61d5c99d587258
Manuel Escobar <manemawi@gmail.com>
2023-10-25 20:22:10 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -77,7 +77,11 @@ class ComunaController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+            ->orderBy("muni_nomb")
+            ->get();
+        return view("comunas.edit", ["comunas" => $comunas, "municipios"=>$municipios]);
     }
 
     /**
@@ -89,7 +93,18 @@ class ComunaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $comuna->comu_nomb = $request->name;
+        $comuna->comu_codi = $request->code;
+        $comuna->save();
+
+        $comuna = DB::table('tb_comuna')
+        ->join("tb_municipio", "tb_comuna.muni_codi", "=", "tb_municipio.muni_codi")
+        ->select("tb_comuna.*", "tb_municipio.muni_nomb")
+        ->get();
+
+        return view("comunas.index", ["comunas" => $comunas]);
     }
 
     /**
